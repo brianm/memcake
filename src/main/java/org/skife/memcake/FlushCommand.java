fa1953bbbd528e28fab4d71dbef4b1d8facc9e90
Brian McCallister <brianm@skife.org>
2017-02-18 21:41:32 -0800

    simplify flush

--- src/main/java/org/skife/memcake/FlushCommand.java
+++ src/main/java/org/skife/memcake/FlushCommand.java
@@ -21,25 +21,15 @@ class FlushCommand extends Command {
     }
 
     @Override
-    public void write(Connection conn, Integer opaque) {
-        byte extraLength = (byte) (expires > 0 ? 4 : 0);
-        ByteBuffer buffer = ByteBuffer.allocate(24 + extraLength);
-        buffer.put(Bits.CLIENT_MAGIC);
-        buffer.put(opcode());
-        buffer.put((byte) 0x00); // key length 1
-        buffer.put((byte) 0x00); // key length 1
-        buffer.put(extraLength); // extra length
-        buffer.put((byte) 0x00); // data type
-        buffer.putChar((char) 0x00); // vbucket
-        buffer.putInt(extraLength); // totalBody
-        buffer.putInt(opaque);
-        buffer.putLong(0); //cas
-        if (expires > 0) {
+    byte extraLength() {
+        return (byte) (expires > 0 ? 4 : 0);
+    }
+
+    @Override
+    void writeBody(ByteBuffer buffer) {
+        if (expires >0) {
             buffer.putInt(expires);
         }
-
-        buffer.flip();
-        Command.writeBuffer(conn, buffer);
     }
 
     @Override
