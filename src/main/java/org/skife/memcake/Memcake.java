295e03bf24e768dda0f5c52a54f19f39d24d3859
Brian McCallister <brianm@skife.org>
2017-03-10 16:29:33 -0800

    remove unused import

--- src/main/java/org/skife/memcake/Memcake.java
+++ src/main/java/org/skife/memcake/Memcake.java
@@ -26,7 +26,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Function;
